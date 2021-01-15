697a2ae84c963f8df4c65d8411f858459e91bf0c
cbw29512 <divclass01@gmail.com>
2021-01-14 22:56:43 -0500

    form needs to be validated with netlify

---
+++ script.js
@@ -0,0 +1,11 @@
+$(document).ready( function() {
+    var now = new Date();
+    var today = now.getFullYear() + '-' + (now.getMonth() + 1) + '-' + now.getDate();
+    $('#datePicker1').val(today);
+});
+
+$(document).ready( function() {
+    var now = new Date();
+    var today = now.getFullYear() + '-' + (now.getMonth() + 1) + '-' + now.getDate();
+    $('#datePicker2').val(today);
+});
